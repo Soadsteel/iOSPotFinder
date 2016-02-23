d6564c1f7f8f25cdc8b11d3c602e8b6375836c9b
Christopher O'Dell <christopher.odell@slalom.com>
2016-02-23 12:44:21 -0800

    Initial Commit

---
+++ PotFinder/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PotFinder
+//
+//  Created by Christopher O'Dell on 2/23/16.
+//  Copyright © 2016 Christopher O'Dell. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PotFinder/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PotFinder
+//
+//  Created by Christopher O'Dell on 2/23/16.
+//  Copyright © 2016 Christopher O'Dell. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
